619cc18999a573e2eb26ba36997964db839eae2b
Goddess <secretgoddess6666@gmail.com>
2025-04-19 16:28:01 +0100

    update: minor code improvements

--- app/app.py
+++ app/app.py
@@ -13,16 +13,20 @@ if uploaded_file:
     st.subheader("Raw Data")
     st.dataframe(df.head())
 
-if 'Date' in df.columns:
-    df['Date'] = pd.to_datetime(df['Date'])
-    df = df.sort_values('Date')
+    if 'Date' in df.columns:
+        df['Date'] = pd.to_datetime(df['Date'])
+        df = df.sort_values('Date')
 
-st.line_chart(df.set_index("Date")["Close"])
+        st.line_chart(df.set_index("Date")["Close"])
 
-st.subheader("Closing price over time")
-plt.figure(figsize=(10, 4))
-plt.plot(df['Date'], df['Close'], label='Close', color='blue')
-plt.xlabel("Date")
-plt.ylabel("Price")
-plt.title("Crypto Price Trend")
-st.pyplot(plt) 
+        st.subheader("Closing price over time")
+        plt.figure(figsize=(10, 4))
+        plt.plot(df['Date'], df['Close'], label='Close', color='blue')
+        plt.xlabel("Date")
+        plt.ylabel("Price")
+        plt.title("Crypto Price Trend")
+        st.pyplot(plt) 
+    else:
+        st.warning("This CSV has no 'Date' columns.")
+else:
+    st.info("Please upload a CSV file to get started")
