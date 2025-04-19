3e157e500e5b7be905891c96d1d1be2b9b6b89a0
Goddess <secretgoddess6666@gmail.com>
2025-04-19 18:56:01 +0100

    troubleshoot

--- app/app.py
+++ app/app.py
@@ -19,24 +19,21 @@ if uploaded_file:
         df = df.sort_values('date')
 
         st.line_chart(df.set_index("date")["price"])
+        df['sma_20'] = df['price'].rolling(window=20).mean()
+        df['sma_100'] = df['price'].rolling(window=100).mean()
 
+        st.subheader("📈 Price with Moving Averages")
+
+        plt.figure(figsize=(10, 4))
+        plt.plot(df['date'], df['price'], label="Close Price")
+        plt.plot(df['date'], df['sma_20'], label="SMA 20", linestyle='--')
+        plt.plot(df['date'], df['sma_100'], label="SMA 100", linestyle=':')
+        plt.xlabel("Date")
+        plt.ylabel("Price")
+        plt.legend()
+        plt.title("Price + Moving Averages")
+        st.pyplot(plt)
     else:
         st.warning("This CSV has no 'snapped_at' columns.")
 else:
     st.info("Please upload a CSV file to get started")
-
-df['sma_20'] = df['price'].rolling(window=20).mean()
-df['sma_100'] = df['price'].rolling(window=100).mean()
-
-st.subheader("📈 Price with Moving Averages")
-
-plt.figure(figsize=(10, 4))
-plt.plot(df['date'], df['price'], label="Close Price")
-plt.plot(df['date'], df['sma_20'], label="SMA 20", linestyle='--')
-plt.plot(df['date'], df['sma_100'], label="SMA 100", linestyle=':')
-plt.xlabel("Date")
-plt.ylabel("Price")
-plt.legend()
-plt.title("Price + Moving Averages")
-st.pyplot(plt)
-
