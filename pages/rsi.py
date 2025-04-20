9308f21da60b0cd169e3d7ebeca7dad7573d7eb4
Goddess <secretgoddess6666@gmail.com>
2025-04-20 02:12:35 +0100

    testing st.button()

--- pages/rsi.py
+++ pages/rsi.py
@@ -4,3 +4,6 @@ if "df" in st.session_state:
     df = st.session_state["df"]
 else:
     st.warning("No data uploaded yet. Please upload from the raw Data page")
+
+st.button("button 1")
+st.button("button 2")
